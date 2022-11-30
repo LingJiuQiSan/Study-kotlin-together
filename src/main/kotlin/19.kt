00c949e6fe6cf32c96f3f809cb94e3ee8d3cce89
LingSan_CanMeng <yxp20050828@163.com>
2022-12-01 00:27:32 +0800

    第二课

--- src/main/kotlin/19.kt
+++ src/main/kotlin/19.kt
@@ -1,6 +1,13 @@
 fun main(args: Array<String>) {
     val x = 3
     val y = 1
+    /*
+    if (x > y){
+        println("x比y大")
+    } else {
+        println("x不比y大")
+    }
+    */
     println(if (x > y) "x比y大" else "x不比y大")
     println("无论怎么样这一行都将会被执行")
 }
---
+++ src/main/kotlin/35.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    //整型
+    var x = 1
+    var hugeNumber = 6L
+    //浮点数
+    var y = 123.5
+    var z = 123.5F
+    //布尔类型
+    var isBarking = true
+    var isTrained = false
+    //字符
+    var letter = 'D'
+    //字符串
+    var name = "Fido"
+}
